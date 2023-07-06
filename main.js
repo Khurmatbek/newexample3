c831c4614db4660e84427fb5973ed543c7258d57
Khurmatbek <xurmatbekolloyorov1514@gmail.com>
2023-07-06 19:54:00 +0500

    add new class

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 var ElBtn = document.querySelector(".hero__header-btn-dark");
 ElBtn.addEventListener("click", function () {
     document.body.classList.toggle("dark");
+})
 
-}
-)
+
+var Btn= document.querySelector(".hero__header-btn");
+Btn.addEventListener("click", function(){
+    document.body.classList.toggle("open")
+})
 
 
 
