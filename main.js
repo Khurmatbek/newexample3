ab43dcc1f9ac96c1c7bd93baf6a699c7a7e90aab
Khurmatbek <xurmatbekolloyorov1514@gmail.com>
2023-07-07 11:50:40 +0500

    add corusel

--- main.js
+++ main.js
@@ -8,6 +8,42 @@ var Btn= document.querySelector(".hero__header-btn");
 Btn.addEventListener("click", function(){
     document.body.classList.toggle("open")
 })
+$(document).ready(function(){
+    $('.customers__list').slick({
+        dots: true,
+        infinite: false,
+        speed: 600,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 900,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+  });
+
+
 
 
 
